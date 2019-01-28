500c401d267a504ab505675090bf7086f2dff566
rob <robertaxelkirby@gmail.com>
2019-01-28 16:23:21 +0000

    Complete more of the next.js tutorial

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
-const Index = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
+import Layout from '../components/MyLayout';
+import Link from 'next/link';
+
+const PostLink = (props) => (
+  <li>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
 );
 
-export default Index;
+export default () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink id="hello-nextjs" title="Hello Next.js" />
+      <PostLink id="another-title" title="Another title" />
+      <PostLink id="title-again-test" title="Title again" />
+    </ul>
+  </Layout>
+)
