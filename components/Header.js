a231426f16262148a782a46c77688245d6f017c0
rob <robertaxelkirby@gmail.com>
2019-01-29 14:33:23 +0000

    Add logo to header

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,18 @@ import Link from 'next/link';
 const titleBarStyle = {
   height: '50px',
   width: '100%',
-  background: 'green',
   userSelect: 'none',
 }
 
+const logoStyle = {
+  height: '50px',
+  marginLeft: '10px'
+}
+
 const Header = () => (
-  <div style={titleBarStyle}/>
+  <div style={titleBarStyle}>
+    <img src='./static/dynium-logo.gif' style={logoStyle}/>
+  </div>
 );
 
 export default Header;
