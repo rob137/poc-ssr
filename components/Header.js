69774f988b7586b22e1f479178c2ffdd3b59dca6
rob <robertaxelkirby@gmail.com>
2019-01-29 14:21:53 +0000

    Adjust Header styles

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,10 @@
 import Link from 'next/link';
 
 const titleBarStyle = {
-  position: 'fixed',
-  display: 'flex',
-  alignItems: 'center',
   height: '50px',
   width: '100%',
   background: 'green',
-  margin: 0,
   userSelect: 'none',
-  top: 0,
-  left: 0
 }
 
 const Header = () => (
