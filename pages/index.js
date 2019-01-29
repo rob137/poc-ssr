3c000383ce89b290778c24a60f5c0cf73fddbcaf
rob <robertaxelkirby@gmail.com>
2019-01-29 10:28:57 +0000

    Prep for deployment with ZEIT now

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
 import Layout from '../components/MyLayout';
 import Link from 'next/link';
 
-const PostLink = (props) => (
+function getPosts() {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js' },
+    { id: 'learn-nextjs', title: 'Learn Next.js' },
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
+  ];
+}
+
+const PostLink = ({ post }) => (
   <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
+    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+      <a>{post.title}</a>
     </Link>
+    <style jsx>{`
+      li{
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 );
 
@@ -13,9 +36,16 @@ export default () => (
   <Layout>
     <h1>My Blog</h1>
     <ul>
-      <PostLink id="hello-nextjs" title="Hello Next.js" />
-      <PostLink id="another-title" title="Another title" />
-      <PostLink id="title-again-test" title="Title again" />
+      {getPosts().map((post) => <PostLink key={post.id} post={post} />)}
     </ul>
+    <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+    `}</style>
   </Layout>
 )
