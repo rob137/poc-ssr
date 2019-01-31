9dcb5cfb963ac017119263af3648dd35398ada71
rob <robertaxelkirby@gmail.com>
2019-01-31 12:15:11 +0000

    Add next-sass to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const webpack = require('webpack');
-module.exports = {
+const withSass = require('@zeit/next-sass');
+
+module.exports = withSass({
   webpack: (config) => {
     config.plugins.push(
       new webpack.EnvironmentPlugin(process.env)
     );
     return config;
   }
-}
+});
