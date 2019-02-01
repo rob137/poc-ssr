5c7d1252dce70052f862481c9146390289bc11f4
rob <robertaxelkirby@gmail.com>
2019-02-01 15:25:56 +0000

    Restructure index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,9 @@
 import Layout from '../components/Layout';
-import MapView from '../components/MapView';
 import SidePanel from '../components/SidePanel';
 import '../static/Index.scss';
 
-const style = {
-  height: '100%',
-  width: '100%'
-};
-
 export default () => (
-  <div style={style}>
-    <Layout>
-      <MapView />
-      <SidePanel />
-    </Layout>
-  </div>
+  <Layout>
+    <SidePanel />
+  </Layout>
 )
