fcafe62eb9724d58fe5dff651e4c029f986e39ff
rob <robertaxelkirby@gmail.com>
2019-02-04 10:51:32 +0000

    Git display map

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Layout from '../components/Layout';
 import SidePanel from '../components/SidePanel';
+import MapView from '../components/MapView';
 import '../static/Index.scss';
 
 export default () => (
   <Layout>
     <SidePanel />
+    <MapView />
   </Layout>
 )
